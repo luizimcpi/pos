bd23ee8023696562be736e92d860c24dada33f5d
Luiz Henrique <luizhenrique.se@gmail.com>
2025-02-21 00:58:30 -0300

    Update Tables.jsx

--- pos-frontend/src/pages/Tables.jsx
+++ pos-frontend/src/pages/Tables.jsx
@@ -1,19 +1,35 @@
 import BackButton from "../components/shared/BackButton";
 import BottomNav from "../components/shared/BottomNav";
 import TableCard from "../components/tables/TableCard";
+import { useState } from "react";
+import { tables } from "../constants";
 
 const Tables = () => {
+
+    const [status, setStatus] = useState('all');
+
     return (
-       <section className="bg-[#f1f1f1] h-[calc(100vh-5rem)] overflow-hidden">
+       <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden">
             <div className="flex items-center justify-between px-10 py-4">
                 <div className="flex items-center gap-4">
-                <BackButton />
-                <h1 className="text-[#f5f5f5] text-2x1 font-bold tracking-wider">Tables</h1>
+                    <BackButton />
+                    <h1 className="text-[#f5f5f5] text-2x1 font-bold tracking-wider">Tables</h1>
                 </div>
-                <TableCard />
-
+                <div className="flex items-center justify-around gap-4">
+                    <button onClick={() => setStatus("all")} className={`text-[#ababab] text-lg ${status === "all" && "bg-[#383838] rounded-lg  px-5 px-2"} rounded-lg px-5 py-2 font-semibold`}>All</button>
+                    <button onClick={() => setStatus("booked")} className={`text-[#ababab] text-lg ${status === "booked" && "bg-[#383838] rounded-lg  px-5 px-2"}  bg-[#383838] rounded-lg px-5 py-2 font-semibold`}>Booked</button>
+                </div>
             </div>
-          <BottomNav />
+            <div className="flex flex-wrap gap-5 p-10">
+              {
+                tables.map((table) => {
+                    return (
+                        <TableCard key={table.id} name={table.name} status={table.status} initials={table.initial}/>
+                    )
+                })
+              }
+            </div>
+            <BottomNav />
         </section>
     ) ;
 }; 
